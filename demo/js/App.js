06957b1d6bb032e5353bfee6fbc0625d72470ebc
Matis Lepik <matis.lepik@gmail.com>
2018-11-02 13:45:24 +0200

    Render demo

--- demo/js/App.js
+++ demo/js/App.js
@@ -1,12 +1,43 @@
 /* @flow */
 import React from 'react';
+import { Countdown, BaseCountdown } from '../../src';
+import theme from '../../src/theme.css';
 
 export type Props = {};
 
 export default class App extends React.Component {
   props: Props;
 
+  constructor(props) {
+    super(props);
+
+    const date = new Date();
+    date.setYear(date.getFullYear() + 1);
+
+    this.state = {
+      date,
+    };
+  }
+
   render() {
-    return <div>Hello world!</div>;
+    return (
+      <div>
+        <link rel="stylesheet" href={theme} />
+
+        <h1>Countdown</h1>
+        <Countdown target={this.state.date.toISOString()} />
+
+        <h1>BaseCountdown</h1>
+        <BaseCountdown target={this.state.date.toISOString()}>
+          {({ segments }) => (
+            <div className="countdown">
+              <span>{segments.days} days,&nbsp;</span>
+              <span>{segments.hours} hours,&nbsp;</span>
+              <span>{segments.minutes} minutes</span>
+            </div>
+          )}
+        </BaseCountdown>
+      </div>
+    );
   }
 }
